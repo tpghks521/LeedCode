f4b9ea1a2d12161e2b58080f1c2c1994d350c4f3
Alex <tpghks521@gmail.com>
2022-08-07 18:55:28 +0900

    fisrt commit

---
+++ HelloApplication.kt
@@ -0,0 +1,20 @@
+package com.leetcode.leo.leetcodeproject
+
+
+fun main() {
+    val array = intArrayOf(2, 3, 4)
+    twoSum(array, 5)
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val resultArray = IntArray(2)
+    for (x in 0..nums.lastIndex) {
+        for (y in x + 1..nums.lastIndex) {
+            if (nums[x] + nums[y] == target) {
+                resultArray[0] = x
+                resultArray[1] = y
+            }
+        }
+    }
+    return resultArray
+}
